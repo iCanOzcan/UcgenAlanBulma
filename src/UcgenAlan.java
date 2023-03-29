e73759adca5f2770e41c2c9f897b920dcc4cee16
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-29 09:01:28 +0300

    Update UcgenAlan.java

--- src/UcgenAlan.java
+++ src/UcgenAlan.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class UcgenAlan {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int a,b,c;
         double area,u;
